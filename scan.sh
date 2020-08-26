e07f324f885f612bb6511b90ba705545eb7c4726
mikesclassicads <65792535+mikesclassicads@users.noreply.github.com>
2020-08-26 12:52:48 -0500

    Create scan.sh

---
+++ scan.sh
@@ -0,0 +1,4 @@
+#script to scan images
+#add "read" command to set image prefix and suffix
+#add "read" command to set dpi
+#add "wait" or "sleep" command before setting a key press that will go ahead with next scan
