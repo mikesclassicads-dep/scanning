ef164163508684bda32fd27fba02b9a860ba8e34
mikesclassicads <65792535+mikesclassicads@users.noreply.github.com>
2020-08-28 09:59:19 -0500

    Adding working starting point.

--- scan.sh
+++ scan.sh
@@ -2,3 +2,5 @@
 #add "read" command to set image prefix and suffix
 #add "read" command to set dpi
 #add "wait" or "sleep" command before setting a key press that will go ahead with next scan
+#this command works as expected
+scanimage --format=pnm --mode=color --progress --resolution=100 > set1-tsep19470412-test.ppm
