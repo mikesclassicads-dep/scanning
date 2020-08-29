e1acfb09c12ba1f26c8d152aab28de6ec3498a48
mikesclassicads <65792535+mikesclassicads@users.noreply.github.com>
2020-08-29 15:03:42 -0500

    Update pnm-to-tif.sh

--- pnm-to-tif.sh
+++ pnm-to-tif.sh
@@ -1,3 +1,6 @@
 #convert pnm (pmm) format to tif/tiff and set actual ppi
 #requires imagemagick
 #convert -units PixelsPerInch <image.file> -density <resolution ppi> <output.file>
+for f in *.pnm; do
+convert -units PixelsPerInch "$f" -density 600 "$f".tif;
+done
