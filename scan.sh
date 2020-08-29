52d6f313865dc59731838963c1a55bd85a83608c
mikesclassicads <mike@mikesclassicads.com>
2020-08-29 12:27:18 -0500

    Update

--- scan.sh
+++ scan.sh
@@ -1,4 +1,4 @@
-0#!/bin/bash
+#!/bin/bash
 read -p 'What format do you want to use? Select png, jpg, pnm, ppm, tiff ' format
 read -p 'What do you want the filename prefix to be? ' prefix
 read -p 'Pick a dpi/ppi resolution ' resolution
@@ -7,7 +7,7 @@ read -p 'Pick a dpi/ppi resolution ' resolution
 scanimage --format=$format --mode=color --progress --resolution=$resolution > $prefix-$resolutiondpi-$(date +%s).$format
 echo "Do you want to scan another page?"
 #bash is drawing all over itself so lets see if 'wait' will stop that
-wait 3
+wait 1
 while true;
 do
 #do I put a "for loop" here if I want to increment the output name?
@@ -17,7 +17,7 @@ do
        #scanimage --format=$format --mode=color --progress --resolution=$resolution > $prefix-$(date +%s).$format
 	scanimage --format=$format --mode=color --progress --resolution=$resolution > $prefix-$resolutiondpi-$(date +%s).$format
        #bash draws on itself when the 'true' statement loops, see if 'wait' will fix 
-        wait 3
+        wait 1
     else
         exit 0
     fi
