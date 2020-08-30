a3f2224595c44e56fd3306ad666b7364a46f0cf5
mikesclassicads <65792535+mikesclassicads@users.noreply.github.com>
2020-08-29 19:34:27 -0500

    Smarter now at naming output filenames.

--- pnm-to-tif.sh
+++ pnm-to-tif.sh
@@ -2,5 +2,5 @@
 #requires imagemagick
 #convert -units PixelsPerInch <image.file> -density <resolution ppi> <output.file>
 for f in *.pnm; do
-convert -units PixelsPerInch "$f" -density 600 "$f".tif;
+convert -units PixelsPerInch "$f" -density 600 "${f%.*}.tif"
 done
